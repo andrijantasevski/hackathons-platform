25797cef3d8fdb62f9e32e7df025bd8cbc8bea78
Andrijan Tasevski <andrijantasevski@gmail.com>
2023-05-13 13:24:24 +0200

    Underlined input completed.

--- hackathon-project/src/pages/index.tsx
+++ hackathon-project/src/pages/index.tsx
@@ -1,14 +1,17 @@
 import { Button } from "@/components/ui/Button";
+import Input from "@/components/ui/Input";
 
 export default function Home() {
   return (
-    <main>
-      <Button intent="primary" size="medium">
-        Hello World!
-      </Button>
-      <Button intent="error" size="medium">
-        Hello WOrld!
-      </Button>
+    <main className="p-4">
+      <div>INPUT STATES FOR UNDERLINED INPUT</div>
+      <Input intent="primary" errorMessage="Error message for nameInput" id="nameInput" type="text" placeholder="Hi">
+        Label
+      </Input>
+
+      <Input intent="error" errorMessage="Error message for surnameInput" id="surnameInput" type="text" placeholder="Hi">
+        Hi
+      </Input>
     </main>
   );
 }
