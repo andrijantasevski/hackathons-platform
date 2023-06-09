938ffda74af83c3ee81f4b2e4845c5f4a29d7f55
Gjorgji <gorgi_@live.com>
2023-06-10 01:15:02 +0200

    refactor: separator component radix

--- src/components/ui/Separator.tsx
+++ src/components/ui/Separator.tsx
@@ -1,3 +1,28 @@
-export default function Separator() {
-  return <div className="h-0.5 w-full rounded-lg bg-gray-300" />;
-}
+import * as React from "react"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+import { twMerge } from "tailwind-merge"
+
+const Separator = React.forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(
+  (
+    { className, orientation = "horizontal", decorative = true, ...props },
+    ref
+  ) => (
+    <SeparatorPrimitive.Root
+      ref={ref}
+      decorative={decorative}
+      orientation={orientation}
+      className={twMerge(
+        "shrink-0 bg-neutral-300",
+        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        className
+      )}
+      {...props}
+    />
+  )
+)
+Separator.displayName = SeparatorPrimitive.Root.displayName
+
+export { Separator }
